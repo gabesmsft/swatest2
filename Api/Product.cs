8b9ac8cbf9fc3b731ed29fa542ac6c08fae07256
Gabe Shapiro <gabes@microsoft.com>
2022-07-27 17:15:33 -0700

    test pre-built func api

--- Api/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Data;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int Quantity { get; set; }
-}
